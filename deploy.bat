c05e4785d3691f0cfc899b79a79d63e4da8d9e54
HoseinGhanbari <DrHoseinGhanbari@gmail.com>
2020-10-31 23:03:34 +0330

    up

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 call ng build --prod --output-path docs --base-href /web-development-course/
 call cp ./docs/index.html ./docs/404.html
 call git add .
-call git commit -m "up"
+call git commit -m "deploy"
 call git push origin master
