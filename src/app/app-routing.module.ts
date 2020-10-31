173be340107929a1765637264194173c44067cc6
HoseinGhanbari <DrHoseinGhanbari@gmail.com>
2020-10-31 22:00:25 +0330

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IntroComponent } from './intro/intro.component';
+import { TopicComponent } from './topic/topic.component';
 
 const routes: Routes = [
-  { path: 'intro', component: IntroComponent },
+  { path: '/intro', component: IntroComponent },
+  {
+    path: '/topic/:topic',
+    component: TopicComponent,
+  },
   {
     path: '**',
     redirectTo: 'intro',
--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
-  styleUrls: ['./topic.component.scss']
+  styleUrls: ['./topic.component.scss'],
 })
-export class TopicComponent implements OnInit {
-
-  constructor() { }
+export class TopicComponent implements OnInit, OnDestroy {
+  topic: string = null;
+  private paramSub: Subscription;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.paramSub = this.route.params.subscribe((params) => {
+      this.topic = params['topic'];
+    });
   }
 
+  ngOnDestroy(): void {
+    this.paramSub.unsubscribe();
+  }
 }
